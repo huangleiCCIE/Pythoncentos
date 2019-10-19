bf4ad4db95119406b1a5196339a0ba1481011a42
hl <2420800264@qq.com>
2019-10-19 17:56:20 +0800

    first day

---
+++ Python_First.py
@@ -0,0 +1,10 @@
+# result= 1+ 2
+# print('计算1 + 2的结果')
+# print('计算结果为:'+ str(result))
+
+# import random
+# a= random.randint(0,255)
+# b= random.randint(0,255)
+# c= random.randint(0,255)
+# d= random.randint(0,255)
+# print(str(a)+ '.'+ str(b)+ '.'+ str(c)+ '.'+ str(d))
